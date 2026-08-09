04bd0aa222c2934bdf58d7baa3c0475b6f486165
Jaishikha Shekhawat <jaishikha99@gmail.com>
2026-08-09 10:09:51 +0530

    Time: 11 ms (82.42%), Space: 22.7 MB (33.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,14 +2,15 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagram_map = defaultdict(list)
-        res = []
+        # res = []
 
         for s in strs:
             sorted_s = tuple(sorted(s))
             anagram_map[sorted_s].append(s)
 
-        for val in anagram_map.values():
-            res.append(val)
+        # for val in anagram_map.values():
+        #     res.append(val)
 
-        return res
+        # return res
+        return list(anagram_map.values())
         
