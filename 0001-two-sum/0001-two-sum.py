941da43951feef6844578bfa60663a9cc9918f83
Jaishikha Shekhawat <jaishikha99@gmail.com>
2026-08-13 22:33:52 +0530

    Time: 3 ms (53.69%), Space: 20.4 MB (58.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,12 @@
+from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
+        mpp = defaultdict(int)
+        for i in range(n):
+            rem = target - nums[i]
+            if rem in mpp:
+                return [mpp[rem], i]
+            mpp[nums[i]] = i
 
-        for i in range(n-1):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-                    
         return []
