2c183ca8800a022c0ad862d2a8eb62151b27bd4e
Jaishikha Shekhawat <jaishikha99@gmail.com>
2026-08-17 00:03:11 +0530

    Time: 208 ms (24.17%), Space: 20 MB (12.18%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,8 +3,8 @@ class Solution:
         l = 0
         maxLen = 0
         mpp = set()
-        for r, ch in enumerate(s):
-            while ch in mpp:
+        for r in range(len(s)):
+            while s[r] in mpp:
                 mpp.remove(s[l])
                 l += 1
 
