19ec3b160b229f7b098d46137b2852449600d760
Jaishikha Shekhawat <jaishikha99@gmail.com>
2026-08-17 08:11:46 +0530

    Time: 25 ms (44.7%), Space: 25.1 MB (94.25%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,14 +3,13 @@ class Solution:
         n = len(nums)
         res = [0] * n
         res[0] = 1
-        for i in range(1, n):
-            res[i] = res[i - 1] * nums[i - 1]
+        for i in range(1,n):
+            res[i] = res[i-1] * nums[i-1]
 
         rightProd = 1
 
-        for r in range(n-1,-1,-1):
-            res[r] *=  rightProd
-            rightProd *= nums[r]
+        for j in range(n-1,-1,-1):
+            res[j] *= rightProd
+            rightProd *= nums[j]
 
         return res
-        
