65a7cb4473d59b9e62f1bc7b9165d2ef42b20d79
Jaishikha Shekhawat <jaishikha99@gmail.com>
2026-08-20 20:54:36 +0530

    Time: 50 ms (19.83%), Space: 19.1 MB (66.02%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -5,7 +5,9 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         low = 1
         high = n
-        while low < high:
+        while low <= high:
+            if isBadVersion(low):
+                return low
             mid = (low + high)//2
             if isBadVersion(mid):
                 high = mid
