681c536002869b24b869870062d5a8c137baebca
Jaishikha Shekhawat <jaishikha99@gmail.com>
2026-08-27 10:00:03 +0530

    Time: 23 ms (80.84%), Space: 53.5 MB (25.01%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,17 +5,39 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        list_val = []
+        stack = []
         curr = head
         while curr:
-            list_val.append(curr.val)
+            stack.append(curr.val)
             curr = curr.next
 
-        left = 0
-        right = len(list_val) - 1
-        while left < right and list_val[left] == list_val[right]:
-            left += 1
-            right -= 1
-        return left >= right
+        curr = head
+        while curr and curr.val == stack.pop():
+            curr = curr.next
+        return curr is None
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # list_val = []
+        # curr = head
+        # while curr:
+        #     list_val.append(curr.val)
+        #     curr = curr.next
+
+        # left = 0
+        # right = len(list_val) - 1
+        # while left < right and list_val[left] == list_val[right]:
+        #     left += 1
+        #     right -= 1
+        # return left >= right
 
     
