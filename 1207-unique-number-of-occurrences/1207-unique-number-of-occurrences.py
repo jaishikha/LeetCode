8cf05b0ea4657d743954579f11939020379af14b
Jaishikha Shekhawat <jaishikha99@gmail.com>
2026-08-28 07:28:38 +0530

    Time: 0 ms (100%), Space: 19.3 MB (77.51%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,6 +1,14 @@
 from collections import Counter
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
+        seen = set()
         mpp = Counter(arr)
-        return len(mpp) == len(set(mpp.values()))
-        
+        for freq in mpp.values():
+            if freq in seen:
+                return False
+            seen.add(freq)
+        return True
+
+
+        # mpp = Counter(arr)
+        # return len(mpp) = len(set(mpp.values()))
