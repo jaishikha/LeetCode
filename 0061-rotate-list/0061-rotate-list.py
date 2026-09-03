18145190dc4175709072ef250a4d75fc4cc1fe86
Jaishikha Shekhawat <jaishikha99@gmail.com>
2026-09-03 18:02:13 +0530

    Time: 0 ms (100%), Space: 19.4 MB (40.85%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -8,32 +8,24 @@ class Solution:
         if head is None or head.next is None:
             return head
 
-        list_val = []
         curr = head
-        while curr:
-            list_val.append(curr.val)
+        length = 1
+
+        while curr.next:
             curr = curr.next
+            length += 1
 
-        n = len(list_val)
-        k %= n
-        def rotate(l, r):
-            # l = 0
-            # r = n - 1
+        k %= length
 
-            while l <= r:
-                list_val[l], list_val[r] = list_val[r], list_val[l]
-                l += 1
-                r -= 1
+        if k == 0:
+            return head
 
-        rotate(0,n-1)
-        rotate(0,k-1)
-        rotate(k,n-1)
+        curr.next = head   
+        newCurr = head
+        for _ in range(1, (length - k)):
+            newCurr = newCurr.next
 
-        curr = head
-        index = 0
-        while curr:
-            curr.val = list_val[index]
-            curr = curr.next
-            index += 1
+        newHead = newCurr.next
+        newCurr.next = None
 
-        return head
+        return newHead
